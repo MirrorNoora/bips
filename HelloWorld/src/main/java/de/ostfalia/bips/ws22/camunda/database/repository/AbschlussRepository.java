581a0caf629d201950c8505cb570dbdd505fda4a
Feiyu Wang <1832518686@qq.com>
2022-11-21 17:36:43 +0100

    abschlussarbeit- antrag repository

--- HelloWorld/src/main/java/de/ostfalia/bips/ws22/camunda/database/repository/AbschlussRepository.java
+++ HelloWorld/src/main/java/de/ostfalia/bips/ws22/camunda/database/repository/AbschlussRepository.java
@@ -1,15 +1,17 @@
 package de.ostfalia.bips.ws22.camunda.database.repository;
 import de.ostfalia.bips.ws22.camunda.database.domain.Abschlussarbeit;
-import de.ostfalia.bips.ws22.camunda.database.domain.Antrag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.sql.Date;
 import java.util.List;
 
-public interface AbschlussRepository extends JpaRepository<Abschlussarbeit, String> {
-    @Query()
-//Arbeit anlegen Enddatum anhand Startdatum berechnen
-    List<Abschlussarbeit> findAllByAntrag(@Param("antrag") Antrag antrag);
-}
 
+public interface AbschlussRepository extends JpaRepository<Abschlussarbeit, Integer> {
+
+    @Query("select a.beginn_datum from Abschlussarbeit a where a.beginn_datum = :date")
+    List<Abschlussarbeit> findAllbyDate(@Param("Datum") Date beginn_datum);
+
+
+}
--- HelloWorld/src/main/java/de/ostfalia/bips/ws22/camunda/database/repository/AntragRepository.java
+++ HelloWorld/src/main/java/de/ostfalia/bips/ws22/camunda/database/repository/AntragRepository.java
@@ -1,8 +1,24 @@
 package de.ostfalia.bips.ws22.camunda.database.repository;
 
+import de.ostfalia.bips.ws22.camunda.database.domain.Abschlussarbeit;
 import de.ostfalia.bips.ws22.camunda.database.domain.Antrag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import sun.util.resources.cldr.en.CalendarData_en_UM;
 
+import java.sql.Date;
+import java.util.List;
 public interface AntragRepository extends JpaRepository<Antrag, Integer> {
-    //Eintrag in DB anlegen(Student + Antrag)
+
+    @Query("select a from Antrag a " +
+            "inner join Professor p ON p.id = a.professor.id " +
+            "where p.id = a.professor.id")
+    List<Antrag> findallbyidprofessor(@Param("Professor") Antrag antrag);
+
+    @Query("select a.typ from Antrag a " +
+            "inner join Abschlussarbeit b ON b.antrag.id = a.id " +
+            "where b.antrag.id = a.id")
+    List<Antrag> findAllByidantrag(@Param("Typ")Integer typ);
+
 }
